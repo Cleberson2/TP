93ba2a522fae12681e76d92c3e928e5f04ceca14
Cleberson2 <58405679+Cleberson2@users.noreply.github.com>
2019-12-01 21:25:49 -0300

    Add files via upload

---
+++ DisciplinaPratica.java
@@ -0,0 +1,21 @@
+package com.dudu;
+
+public class DisciplinaPratica extends Disciplina {
+
+    private int cargaHorariaPratica;
+
+
+    public int getCargaHorariaPratica () {return cargaHorariaPratica; }
+    public void setCargaHorariaPratica (int cargaHorariaPratica) {
+        this.cargaHorariaPratica = cargaHorariaPratica;
+    }
+
+    public DisciplinaPratica () {
+        this.cargaHorariaPratica = 0;
+    }
+
+    public DisciplinaPratica (int cargaHorariaPratica) {
+        this.cargaHorariaPratica = cargaHorariaPratica;
+    }
+}
+
